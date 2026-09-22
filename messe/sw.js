fa7bf0a641fe737acdbe3148e90ea430ad1e3df7
8bibabub8 <8bibabub8@gmail.com>
2026-09-22 17:33:53 +0200

    Add files via upload

--- messe/sw.js
+++ messe/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "messe-hilfe-v2";
+const CACHE = "messe-hilfe-v3";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); self.skipWaiting(); });
 self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
